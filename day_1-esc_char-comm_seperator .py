f9393308654f90d2e525e7dd87b1e9e4c9ec5ca7
1m-crysta1 <crystals.pc.setup@gmail.com>
2025-07-23 12:34:14 +0530

    Add files via upload

---
+++ day_1-esc_char-comm_seperator .py
@@ -0,0 +1,17 @@
+# 1)
+print("hey im \n \"crystal\" " )
+# this is the use of escape sequence characters
+
+# \"crystal\" (if we write anything in this format it will be printed in double  quotes )
+
+# 2)
+'''
+ljhjhkjhjklglkjdhxkljhslouhiosudxhilusdhiu
+dsgjgdhjkygsygdlygkliygdfluygfudygfyg'''
+# the above section represents multi line comment 
+
+# this  is a single line comment
+
+print ("hey",6,7,  sep="~" , end= " crystal\n")
+# THIS IS A SEPERATOR used to seperate the printed text by any symbol we use 
+# AND end syntax is used to represent the ending part of a string or any kind of printing format  
